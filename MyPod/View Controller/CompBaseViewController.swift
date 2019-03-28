9fa275c46ac7c2f01d39b104fee7c60ef4882f72
nihalk <nihalk@softcell.com>
2019-03-28 18:52:38 +0530

    initial commit

---
+++ MyPod/View Controller/CompBaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CompBaseViewController.swift
+//  MyPod
+//
+//  Created by Nihal Khokhari on 28/03/19.
+//  Copyright © 2019 Nihal Khokhari. All rights reserved.
+//
+
+import UIKit
+
+class CompBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MyPod' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MyPod
+
+  target 'MyPodTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end

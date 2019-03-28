bbe639825cd704f0f426ea1649e3a28aa929655e
nihalk <nihalk@softcell.com>
2019-03-28 13:00:22 +0530

    Initial Commit

---
+++ MyPod/MyPod.h
@@ -0,0 +1,19 @@
+//
+//  MyPod.h
+//  MyPod
+//
+//  Created by Nihal Khokhari on 28/03/19.
+//  Copyright © 2019 Nihal Khokhari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyPod.
+FOUNDATION_EXPORT double MyPodVersionNumber;
+
+//! Project version string for MyPod.
+FOUNDATION_EXPORT const unsigned char MyPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPod/PublicHeader.h>
+
+
